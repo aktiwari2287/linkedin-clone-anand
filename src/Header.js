8c8a9c080326c98aacf22fe50c3f34ac15371e1e
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-10 22:00:09 +0530

    Added styled components

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import HeaderOption from './HeaderOption'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1623332815~hmac=1fe72c8745cf818e843b8863b2c7df3b" alt=""/>
+                <div className="header__search">
+                    <SearchIcon></SearchIcon>
+                    <input type="text" name="" id=""/>
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home"></HeaderOption>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"></HeaderOption>
+            </div>
         </div>
     )
 }
