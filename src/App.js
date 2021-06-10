9156a9c9eab681eda51eda18cae0209453d9d734
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-10 18:37:45 +0530

    Added project structure for the linkedin clone

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Building React App</h1>
+        <Header></Header>
+    </div>
+  );
+}
+
+export default App;
